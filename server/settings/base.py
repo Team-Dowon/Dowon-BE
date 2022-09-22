2c8155db1a6d8ef510cf5b944524f23ab45f910d
chwangmin <44563065+chwangmin@users.noreply.github.com>
2022-09-22 20:04:56 +0900

    :Hammer:Modify base.py -> ec2 v.2

--- server/settings/base.py
+++ server/settings/base.py
@@ -34,7 +34,7 @@ DEBUG = env('DEBUG')
 AUTH_USER_MODEL = 'api.User'
 
 ALLOWED_HOSTS = [
-    "http://ec2-54-180-119-169.ap-northeast-2.compute.amazonaws.com/",
+    "ec2-54-180-119-169.ap-northeast-2.compute.amazonaws.com",
     "127.0.0.1"
 ]
 
