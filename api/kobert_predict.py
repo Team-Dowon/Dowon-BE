8810606c23d7a2b483c2e9e5bd3bc4dda3ad9b7e
chwangmin <44563065+chwangmin@users.noreply.github.com>
2022-11-04 00:40:37 +0900

    :goal_net:test model copy8

--- api/kobert_predict.py
+++ api/kobert_predict.py
@@ -1,5 +1,4 @@
 from torch.utils.data import Dataset
-from kobert.pytorch_kobert import get_pytorch_kobert_model
 from kobert.utils import get_tokenizer
 # torch
 import torch
@@ -56,31 +55,20 @@ class BERTClassifier(nn.Module):  ## 클래스를 상속
         return self.classifier(out)
 
 
-class BertModel():
-    # BERT 모델, Vocabulary 불러오기 필수
-    bertmodel, vocab = get_pytorch_kobert_model()
-
-    device = torch.device('cpu')
-    model = BERTClassifier(bertmodel, dr_rate=0.5).to(device)
-    model.load_state_dict(
-        torch.load('/root/model/SentimentAnalysisKOBert_StateDict.pt', map_location='cpu'))
-
-
 class bert_predict(object):
     def __init__(self, predict_sentence):
         self.predict_sentence = predict_sentence
 
-    def new_softmax(logits):
-        c = np.max(logits)  # 최댓값
-        exp_a = np.exp(logits - c)  # 각각의 원소에 최댓값을 뺀 값에 exp를 취한다. (이를 통해 overflow 방지)
-        sum_exp_a = np.sum(exp_a)
-        y = (exp_a / sum_exp_a) * 100
-        return np.round(y, 3)
-
-
     # 예측 모델 설정
     def area(self):
-        vocab = BertModel.vocab
+        from kobert.pytorch_kobert import get_pytorch_kobert_model
+        # BERT 모델, Vocabulary 불러오기 필수
+        bertmodel, vocab = get_pytorch_kobert_model()
+
+        device = torch.device('cpu')
+        model = BERTClassifier(bertmodel, dr_rate=0.5).to(device)
+        model.load_state_dict(
+            torch.load('/root/model/SentimentAnalysisKOBert_StateDict.pt', map_location='cpu'))
 
         # Setting parameters
         max_len = 64
@@ -97,7 +85,7 @@ class bert_predict(object):
         another_test = BERTDataset(dataset_another, 0, 1, tok, max_len, True, False)
         test_dataloader = torch.utils.data.DataLoader(another_test, batch_size=batch_size, num_workers=5)
 
-        BertModel.model.eval()
+        model.eval()
 
         for batch_id, (token_ids, valid_length, segment_ids, label) in enumerate(test_dataloader):
             token_ids = token_ids.long().to(device)
@@ -105,7 +93,7 @@ class bert_predict(object):
 
             valid_length = valid_length
 
-            out = BertModel.model(token_ids, valid_length, segment_ids)
+            out = model(token_ids, valid_length, segment_ids)
 
             for i in out:
                 logits = i
