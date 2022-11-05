cdfaf8d79ee34c5df092d51f9e4c03ba24fd9d3d
chwangmin <44563065+chwangmin@users.noreply.github.com>
2022-11-05 19:21:18 +0900

    :goal_net:test test.view v2

--- api/kobert_predict.py
+++ api/kobert_predict.py
@@ -6,6 +6,9 @@ import gluonnlp as nlp
 import numpy as np
 from torch.utils.data import Dataset
 
+from kobert.pytorch_kobert import get_pytorch_kobert_model
+from kobert.utils import get_tokenizer
+
 
 # KoBERT에 입력될 데이터셋 정리
 class BERTDataset(Dataset):
@@ -60,8 +63,6 @@ class bert_predict(object):
 
     # 예측 모델 설정
     def area(self):
-        from kobert.pytorch_kobert import get_pytorch_kobert_model
-        from kobert.utils import get_tokenizer
         # BERT 모델, Vocabulary 불러오기 필수
         bertmodel, vocab = get_pytorch_kobert_model()
 
