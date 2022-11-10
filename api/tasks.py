2573a6d512f646aadd310c157f52570d9bcde8b9
chwangmin <44563065+chwangmin@users.noreply.github.com>
2022-11-11 01:40:52 +0900

    :goal_net:test celery server v6

--- api/tasks.py
+++ api/tasks.py
@@ -2,6 +2,7 @@
 
 from __future__ import absolute_import, unicode_literals
 from celery import shared_task
+from django.http import JsonResponse
 
 from server.celery import app
 from api.kobert_predict import bert_predict
@@ -9,5 +10,10 @@ from api.kobert_predict import bert_predict
 # test 용 함수
 @shared_task
 def bert_predict_c(sentence):
-    bert_predict(sentence)
-    return None;
+    s_predict = bert_predict_c(sentence)
+    v_predict = s_predict.area()  # sentence 데이터로 감성 예측
+    emotion = v_predict[6]
+    v_predict.pop()
+    percent = max(v_predict)  # 최고 확률 값 확인
+
+    return JsonResponse({'예측값': emotion, '확률': percent}, status=200)  # 프론트로 전달
