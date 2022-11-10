5ad424f907b5edc3f7be2f82b9ce0a9717c51bee
chwangmin <44563065+chwangmin@users.noreply.github.com>
2022-11-10 23:20:28 +0900

    :goal_net:test celery server v2

--- server/settings/base.py
+++ server/settings/base.py
@@ -189,7 +189,7 @@ AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.%s.amazonaws.com' % (AWS_STORAGE_BUCKET_NAME, AWS_REGION)
 
 # Celery
-CELERY_BROKER_URL = env('http://ec2-54-180-119-169.ap-northeast-2.compute.amazonaws.com/')  # 배포용
+CELERY_BROKER_URL = 'amqp://user:pwd@rabbitmq:5672/'  # 배포용
 # CELERY_BROKER_URL = 'amqp://localhost'  # 로컬 테스트용
 CELERY_RESULT_BACKEND = 'django-db'
 CELERY_ACCEPT_CONTENT = ['application/json']
