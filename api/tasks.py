57f3b6f68e0a6b0bfd9ca3930c0fdca4e921c7a8
chwangmin <ckm7907@naver.com>
2022-11-13 19:01:50 +0900

    :goal_net:test celery v9

--- api/tasks.py
+++ api/tasks.py
@@ -8,5 +8,8 @@ from api.kobert_predict import bert_predict
 @shared_task
 def bert_predict_c(sentence):
     s_predict = bert_predict(sentence)
-
-    return s_predict
+    v_predict = s_predict.area()  # sentence 데이터로 감성 예측
+    emotion = v_predict[6]
+    v_predict.pop()
+    percent = max(v_predict)  # 최고 확률 값 확인
+    return Response({'예측값': emotion, '확률': percent}, status=200)  # 프론트로 전달
