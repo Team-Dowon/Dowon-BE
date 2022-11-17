d1c2a14d79527be4c9b9f827117c1d7d7d0d0e44
chwangmin <44563065+chwangmin@users.noreply.github.com>
2022-11-18 00:05:47 +0900

    :bug:Fix bug with celery4

--- server/settings/base.py
+++ server/settings/base.py
@@ -185,11 +185,3 @@ AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
 AWS_REGION = env('BUCKET_REGION')
 AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.%s.amazonaws.com' % (AWS_STORAGE_BUCKET_NAME, AWS_REGION)
-
-# Celery
-CELERY_BROKER_URL = 'amqp://guest:guest@rabbitmq:5672/'  # 배포용
-# CELERY_BROKER_URL = 'amqp://localhost'  # 로컬 테스트용
-CELERY_RESULT_BACKEND = 'django-db'
-CELERY_ACCEPT_CONTENT = ['application/json']
-CELERY_TASK_SERIALIZER = 'json'
-CELERY_RESULT_SERIALIZER = 'json'
