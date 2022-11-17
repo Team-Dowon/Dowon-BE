f57b66e50df9c12e84a7fd6fb631c45eeea072ac
chwangmin <44563065+chwangmin@users.noreply.github.com>
2022-11-18 00:05:26 +0900

    :bug:Fix bug with celery3

--- server/settings/base.py
+++ server/settings/base.py
@@ -52,7 +52,7 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist',
     'storages',
-    'rest_framework',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
